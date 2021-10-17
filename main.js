5d02624eb88e978991d9d9c2e9a0ef414c0c988f
Natedogg0408 <77352991+Natedogg0408@users.noreply.github.com>
2021-10-17 17:42:13 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+right_wristX = 0;
+right_wristY = 0;
+right_wrist_score = 0;
+
 function setup(){
     var canvas = createCanvas(700, 600);
     canvas.parent('canvas');
@@ -12,4 +16,20 @@ function setup(){
 
 function modelLoaded(){
     console.log('poseNet Is Initialized');
+}
+
+function gotPoses(results){
+if(results > 0){
+    right_wristY = results[0].pose.rightWrist.y;
+    right_wristX = results[0].pose.rightWrist.x;
+    right_wrist_score = results[0].keypoints[10].score;
+    console.log(scoreRightWrist);
+}
+}
+
+function draw(){
+    if(right_wrist_score > 0.2){
+        fill("red");
+        stroke("red");
+    }
 }
